d71f35b90ac18cea764993102782d737ec678c6f
김서연 <flowersayo0811@ewhain.net>
2021-09-17 00:31:32 +0900

    [스택,큐,덱] 9월 17일

---
+++ 9월 10일- 스택,큐,덱/10866.cpp
@@ -0,0 +1,178 @@
+﻿#include <iostream>
+#include <vector>
+
+
+using namespace std;
+const int SIZE = 10001;
+
+int front_pointer = 0, rear_pointer = 0;
+vector<int> queue_vec(SIZE);
+
+//empty
+bool empty() {
+    return front_pointer == rear_pointer; // front rear가 같은곳을 가리킬때 큐가 비어있다.
+}
+
+//full
+bool full() {
+    return (rear_pointer + 1) % SIZE == front_pointer;
+}
+
+//push
+void push_back(int k) {
+    rear_pointer = (rear_pointer + 1) % SIZE; //rear포인터를 먼저 하나 증가시키고
+    queue_vec[rear_pointer] = k; //그위치에 k를 삽입
+}
+
+//pop
+int pop_front() { // 큐에서의 default pop
+    front_pointer = (front_pointer + 1) % SIZE; //front포인터를 먼저 하나 증가시키고
+    return queue_vec[front_pointer]; // 그위치에 있는 값 반환.
+
+    //이때 어짜피 비어있는 상태로 치기때문에 값을 굳이 삭제해줄필요 X
+
+}
+
+//size
+int size() { 
+    int tmp = (rear_pointer - front_pointer);
+
+    
+    if (tmp < 0)
+        tmp += SIZE;
+    return tmp;
+}
+
+//front
+int front() { //가장 나중에 들어온 값.
+    int tmp = (front_pointer + 1) % SIZE; //단순히 맨 앞 값을 가져오기만 하는것이므로 front를 증가시킬 필요 X
+    return queue_vec[tmp];
+}
+
+//back
+int back() { //가장 최근에 들어온 값.
+    return queue_vec[rear_pointer]; //rear포인터가 가리키는 값이 가장 최근에 들어온값.
+}
+
+//덱에 추가되는 기능 두가지
+
+//1. 가장 뒤쪽에서 빼기 -> rear 출력 및 rear --
+int pop_back() {
+
+    int temp = rear_pointer--;
+
+    // index 에러 방지.
+    if (rear_pointer  < 0)
+    {
+        rear_pointer += SIZE;
+    }
+   
+
+    return queue_vec[temp];
+}
+
+//2. 가장 앞쪽에 수 삽입하기 -> front출력 및 front -- 
+void push_front(int k) { 
+
+    int temp = front_pointer--;
+
+
+    // index 에러 방지.
+    if (front_pointer  < 0)
+    {
+        front_pointer += SIZE;
+
+    }
+  queue_vec[temp] = k; 
+
+}
+
+int main() {
+
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n, k;
+    string cmd;
+
+
+
+
+    cin >> n;
+    while (n--) {
+        cin >> cmd;
+        if (cmd == "push_front") {
+            cin >> k;
+            if (!full())
+                push_front(k);
+            continue;
+        }
+
+        if (cmd == "push_back") {
+            cin >> k;
+            if (!full()) //라이브러리 사용시 필요 없음
+  
+                push_back(k);
+            continue;
+        }
+        if (cmd == "pop_front") {
+            if (empty())
+                cout << -1 << '\n';
+            else {
+                cout << pop_front() << '\n';
+
+            }
+            continue;
+        }
+
+        if (cmd == "pop_back") {
+            if (empty())
+                cout << -1 << '\n';
+            else
+                cout << pop_back() << '\n';
+            continue;
+        }
+
+        if (cmd == "size") {
+            cout << size() << '\n';
+            continue;
+        }
+        if (cmd == "empty") {
+            cout << empty() << '\n';
+            continue;
+        }
+        if (cmd == "front") {
+            if (empty())
+                cout << -1 << '\n';
+            else
+            {
+                cout << front() << '\n';
+
+            }
+            continue;
+        }
+
+        if (cmd == "back") {
+            if (empty())
+                cout << -1 << '\n';
+            else
+            {
+                cout << back() << '\n';
+
+            }
+            continue;
+
+        }
+    }
+
+    /*
+     * 덱 순회
+     *
+    while (!empty()) {
+        cout << front() << ' ';
+        pop();
+    }
+    */
+}
+
+
---
+++ 9월 10일- 스택,큐,덱/1935.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <stack>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+
+map <char, int> m;// 알파벳 - 숫자 연관지어 저장할 map
+
+
+// 후위표기식을 계산해주는 함수
+double calculate(string str, vector<int> v) { 
+
+	//숫자는 무조건 push, 기호를 만나면 맨 위의 상수 두개를 pop하여 계산한다음 계산한 결과 push.
+
+	stack<double> s;
+	double a=0,b=0,c=0;
+	
+
+	for (int i = 0; i < str.length(); i++) {
+
+		if (str[i] >= 'A' && str[i] <= 'Z') //영문이라면
+		{ 
+
+			s.push(m[str[i]]); //알파벳에 해당하는 숫자로 바꿔서 push
+			//cout << "push"<< m[str[i]];
+		}
+
+		else //기호라면
+		{
+
+			//스택의 맨 위에서 상수 두개를 꺼낸다.
+			a = s.top(); s.pop();
+			b = s.top(); s.pop();
+
+			switch (str[i])
+			{
+
+			case '+':
+				c = a + b;
+				break;
+			case '-':
+				c = b - a;
+				break;
+			case '/':
+				c = b / a;
+				break;
+			case '*':
+				
+				c = a * b;
+				break;
+
+
+			}
+			s.push(c); //계산 결과 c를 스택에 다시 넣는다.
+			//cout << "push" << c;
+		}
+	}
+	return s.top(); // 모든 계산을 마치고 남은 나머지 원소 하나가 연산 결과값이된다.
+
+}
+
+int main() {
+
+	
+
+
+	int n; //피연산자의 개수
+	string str; //후위표기식
+
+	cin >> n;
+
+	cin >> str;
+
+	vector <int> v (n);
+
+	for (int i=0;i< n;i++){
+
+		int n;
+		cin >> n;
+		
+		m['A' + i] = n;
+	}
+	cout << fixed;
+	cout.precision(2);
+	cout << calculate(str, v);
+
+}
