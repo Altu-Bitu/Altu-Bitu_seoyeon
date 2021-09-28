2454f178116cab88a274d293e1d10f6cc4fb5cc2
kimseoyeon <flowersayo0811@ewhain.net>
2021-09-28 23:57:26 +0900

    [브루츠포스] 9월 28일
    추가제출합니다!!

--- 9월 21일-브루트포스/10448.cpp
+++ 9월 21일-브루트포스/10448.cpp
@@ -1,74 +1,64 @@
 #include <iostream>
-#include <string>
-#include <cstring>
-#include <cmath>
+#include <vector>
+
 using namespace std;
 
-#define MAX 10
+const int SIZE = 45; //1000을 넘어가는 삼각수는 필요없으므로 index는 45까지.
+
+vector<int> tri_num(SIZE); //삼각수를 저장할 배열
+
+
+
+//유레카인지 검사하는 반복문 함수
+bool eureka(int num) {
+    for (int i = 1; i <= 45; i++) {
+        for (int j = 1; j <= 45; j++) {
+            for (int k = 1; k <= 45; k++) {
+                if (tri_num[i] + tri_num[j] + tri_num[k] == num) // num이 삼각수의 조합으로 만들어질수 있다면 
+                    return true;//유레카이다.
+            }
+        }
+    }
+
+    return false; //if문에 걸리지 않으면 유레카가 아니다.
+}
+
+/**
+ * [유레카 이론]
+ * 삼각수 T(n) = n * (n + 1) / 2
+ * 어떤 자연수가 정확히 3개의 삼각수의 합으로 표현 가능한지 검사해라
+ *
+ * [풀이]
+ * 어떤 자연수(k)가 주어졌을 때, n=1부터 모든 삼각수 합을 3개씩 조합해보자
+ * 이때, n = 45일 때, T(45) = 45 * 46 / 2 = 1,035 이고, k의 최댓값은 1,000이므로 45의 삼각수까지만 계산하면 됨!
+ * 모두 해봤자, 45 * 45 * 45 < 1억 이므로, 브루트 포스 접근 완전 가능!
+ */
 
 int main() {
+    int t, k;
 
+    //입력
+    cin >> t;
 
-	int n_testcase;
-	
+    //삼각수 SIZE까지 미리 구하기 -> while문을 돌때마다 구하면 너무 비효율적.
+    for (int i = 1; i <= 45; i++)
+        tri_num[i] = i * (i + 1) / 2; //n번째 삼각수 공식 s[n]=n(n+1)/2 .
+    //tri_num[i]에는 i번째 삼각수값이 담긴다.
 
-	cin >> n_testcase;
+    //입력 + 연산 + 출력
+    while (t--) {
+        cin >> k;
 
-	int triangular_numbers[MAX]; //삼각수 저장
+        //반복문 풀이
+        cout << eureka(k) << '\n'; // k가 유레카인지 확인한다.
 
+ 
+    }
 
-	for (int i = 1; i < MAX; i++) {
-
-		triangular_numbers[i] = i * (i + 1) / 2;
-
-	}
-
-
-	/*
-	for (int i = 1; i < MAX; i++) {
-		cout << i << ' ' << triangular_numbers[i];
-	}
-
-	*/
-	int n;
-	while (n_testcase--){
-
-		bool check = false; // 정확히 3개의 삼각수의 합으로 표현될 수 있는지 여부
-		
-		cin >> n;
-
-		int sum;
-		
-		for (int i = 1; i < MAX; i++) {
-
-			for (int j = 1; i < MAX; j++) {
-
-				for (int k = 1; k < MAX; k++) {
-
-
-					sum = triangular_numbers[i] + triangular_numbers[j] + triangular_numbers[k];
-					if (sum == n)
-					{
-						check = true;
-						
-						
-
-					}
-
-
-				}
-			}
-
-			
-		}
-
-		if (check)
-		cout << 1;
-		else
-			cout << 0;
-
-	}
-
-
-
+    return 0;
 }
+
+
+
+
+
--- 9월 21일-브루트포스/2858.cpp
+++ 9월 21일-브루트포스/2858.cpp
@@ -1,48 +1,47 @@
 #include <iostream>
-#include <string>
-#include <cstring>
-#include <cmath>
+
 using namespace std;
 
+//기숙사 바닥의 l(높이), w(넓이) 구해서 리턴하는 함수
+pair<int, int> length(int r, int b) {
+    int area = r + b; // 빨간타일과 갈색타일을 더하면 총 면적.
+    for (int i = area; i > 0; i--) { //i = l(높이) 의 값
+        if (area % i != 0) //w 가 정수가 아니라면 넘어감
+            continue; //정수가 아니라면 area가 두수의 곱으로 나타내어질수없으므로 아래과정을 생략하고 넘어감.
+        int w = area / i; //총 칸 개수 r + b 이므로 l 인 i를 나눈 값이 w(넓이) -> area = r + b = i * w 이기때문.
 
+        if (r == ((i + w) * 2 - 4)) //i * w 라고 가정했을때 필요한 바깥타일의 개수가 가 r 과 같다면
+            return make_pair(i, w); //답이 유일한 경우만 주어진다고 했으므로 i,w 바로 리턴.
+    }
+}
+
+/**
+ * [기숙사 바닥 문제]
+ * 빨간 타일: 테두리를 채움, 갈색 타일: 나머지
+ * 문제 입력에 만족하는 방의 크기 l, w (l > w)를 출력하는 문제
+ *
+ * [풀이]
+ * 기숙사 바닥의 넓이는 타일의 개수의 총합인 r + b
+ * l(높이)이 r + b인 사각형부터 검사 (w = (r + b) / l) -> 테두리 타일 개수가 r개와 같다면 정답
+ * 테두리 개수 = (l + w) * 2 - 4(겹치는 부분)
+ * 입력 범위는 최대 r + b = 2,005,000 이므로 접근 가능!
+ */
 
 int main() {
+    int r, b; //r: 빨간 타일 개수, b: 갈색 타일 개수
 
-	int inside, outside;
-	int L, W;
+    //입력
+    cin >> r >> b;
 
-	int outside_needed;
+    //연산
+    pair<int, int> result = length(r, b); // pair컨테이너에 i,w값 담기.
 
+    //출력
+    cout << result.first << ' ' << result.second << '\n'; //l 과 w 출력
 
-
-	cin >> outside >> inside;
-
-	//inside를 두수의 곱(a * b)으로 나타내기
-	int a, b;
-	for (int i = 1; i <= sqrt(inside); i++) {
-
-		if (inside % i == 0)
-
-			a = i;
-			b = inside / i;
-
-
-		outside_needed = a * 2 + b * 2 + 4;
-
-		if (outside_needed == outside) //inside를 기준으로 필요한 outside 블럭의 개수가 맞아떨어지면,
-		{
-			L = max(a + 2, b + 2);
-			W = min(a + 2, b + 2);
-			break;
-		}
-
-	}
-
-
-
-
-	cout << L << ' ' << W;
-
-
-
+    return 0;
 }
+
+
+
+	
