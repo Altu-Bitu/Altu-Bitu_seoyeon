3fe7f550497ff92fa85dbfbf946d4849cc1eb4a4
kimseoyeon <flowersayo0811@ewhain.net>
2021-10-07 20:34:49 +0900

    [그리디 알고리즘] 피드백반영
    1080은 해결했는데 16206은 테스트 케이스는 다 돌아가는데 아직 반례를 못찾았습니다 ㅠㅠ 한번 오늘 자정까지 고쳐볼게요!

--- 10월 1일- 그리디알고리즘/1080.cpp
+++ 10월 1일- 그리디알고리즘/1080.cpp
@@ -8,46 +8,30 @@ using namespace std;
 
 void reverse(vector < vector <int> >&  matrix, int row, int col) { //행렬 A를 주어진 점으로 부터 3X3 공간을 뒤집는 함수
 
-
 	for (int i = row; i < row+3; i++) {
 
 		for (int j = col; j < col+3; j++) {
 
 			matrix[i][j] = !matrix[i][j]; //toggle
-
 		}
 	}
-
-
 }
 
 
 bool check(vector < vector <int> >& m1, vector < vector <int> >& m2) { //m1,m2가 같은 매트릭스인지 확인
 
-	
-	
 	for (int i = 0; i < m1.size(); i++) {
 
 		for (int j = 0; j < m1[0].size(); j++) {
 
 			if (m1[i][j] != m2[i][j])
 				return false;
-
 		}
-
 	}
-
-
 	return true;
-
 }
 
-
-
 int main() {
-
-
-	
 
 	int r, c;
 
@@ -84,24 +68,21 @@ int main() {
 	int cnt = 0; //연산횟수
 
 	//0,0부터 비교하며 두 행렬의 문자가 다를 경우 바로 바꿔준다. 그리고 한번 바꾼부분은 두번 바꾸지 않는다.
-	for (int i = 0; i < c-2; i++) {
+	for (int i = 0; i < r-2; i++) {
 
-		for (int j = 0; j < r-2; j++) {
-
+		for (int j = 0; j < c-2; j++) {
 
 			if (A[i][j] != B[i][j])
 			{
 				reverse(A, i, j);
 				cnt++;
-
-			}
-
-			if(check(A, B))
-				break; // 한번 변환할때마다 같은지 확인한다...? 
+			}	
 		}
 
 	}
 
-
-	cout << cnt;
+	if (check(A, B)) // 모든 연산을 마친후에 A=B이면
+		cout << cnt; 
+	else // A!=B이면
+		cout << -1;
 }
--- 10월 1일- 그리디알고리즘/16206.cpp
+++ 10월 1일- 그리디알고리즘/16206.cpp
@@ -4,14 +4,18 @@
 
 using namespace std;
 
-bool cmp() {
 
-	//cake배열을 10의 배수가 먼저오도록 정렬할 수 있는 방법?
-	//12 10 20 30 65 40
-	//10 20 30 40 12 65
+// 10의 배수인 케이크부터 먼저잘라야 더 많이 길이가 10인 조각을 만들어낼 수 있다.
+// ->10의배수가 먼저오도록 우선정렬
+bool cmp(const int& a,const int& b) {
+	//cmp가 false를 반환할때 swap됨.
+
+	if (a % 10 == 0 && b % 10 == 0) 
+		return a < b; // 둘다 10의 배수라면 더 작은 순서대로
+
+	return (a % 10 == 0);  //a만 10의배수이면 swap하지 않음.
+
 }
-
-// 10의 배수인 케이크부터 먼저잘라야 더 많이 길이가 10인 조각을 만들어낼 수 있다. 
 
 int main() {
 
@@ -29,16 +33,16 @@ int main() {
 		cin >> cake[i];
 
 	}
-
-	//sort(cake.begin(), cake.end(), cmp);
 	/*
+	sort(cake.begin(), cake.end(), cmp);
+	
 		for (int i = 0; i < n; i++) {
 
 		cout << cake[i] <<' ';
 
 	}
 	*/
-
+	
 	int cnt = 0;// 길이가 10인 케이크 개수
 	int cutting_cnt = 0; //케이스를 자르는 횟수 카운트
 
@@ -55,12 +59,10 @@ int main() {
 	//길이가 10보다 큰 케이크는 될수있는대로 자른다.
 	for (int i = 0; i < n; i++) { 
 		
-		//cout << 8;
 		if (cake[i] <= 10) //10보다 작다면 더자를수 X
 			continue;
 
 		if (cutting_cnt== m) {
-
 			break;
 		}
 			
@@ -77,12 +79,9 @@ int main() {
 		}
 
 		
-		if (rest == 10)
+		if (rest == 10) //10의 배수였다면 조각이 하나 더 추가됨 
 			cnt++;
 		
-		
-		
-	
 	}
 
 	cout << cnt;
